7f12c6e3c2f81f6b84c29e99fe4b88ee2b858736
Charokgb <charokgb@gmail.com>
2025-10-05 23:51:57 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}", "./data/**/*.{js,json}", "./public/**/*.html"],
   theme: {
     extend: {backgroundImage: {
-        'custom-bg': "url('/background.jpg')",
+        'custom-bg': "url('/background.webp')",
       },},
   },
   plugins: [],
